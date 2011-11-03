6a49c4751f966eadb4586ac25f0721bf473268d0
shine Chandran <shine@cmmtechnologies.in>
2011-11-03 14:47:26 +0530

    implemented login checking functionality. added new file login.js for validation

--- index.php
+++ index.php
@@ -1,13 +1,35 @@
+<?php
+require("functions.inc");
+if(isset($_POST['Login']))
+{
+	$connection = dbConnect();
+	$username=$_POST['user_name'];
+	$password=$_POST['password'];
+	if(checkLogin($username,$password,$connection))
+	{
+		//header(""); 
+		echo  "yooooooooooooo";
+		die();
+	}
+	
+
+
+}
+
+?>
+
+
+
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <HTML>
 	<HEAD>
 		<TITLE>LOGIN PAGE</TITLE>
 		<link rel="stylesheet" href="login.css">	
 		<SCRIPT TYPE="TEXT/JAVASCRIPT" SRC="jquery-1.6.2.min.js"></SCRIPT>
-		<SCRIPT TYPE="TEXT/JAVASCRIPT"></SCRIPT>
+	    <SCRIPT TYPE="TEXT/JAVASCRIPT" SRC="login.js"></SCRIPT>
 	</HEAD>
 	<BODY>
-		<SPAN ID="err_msg"></SPAN>
+		
 		<DIV id="wrapper">
 			<DIV ID="logtop">
 				<IMG SRC="images/login_lock.png" width="60px" height="60px" />
@@ -17,15 +39,26 @@
 				
 				<TABLE cellpadding="4">
 				<TR>
-					<TD>Username</TD><TD>: <INPUT TYPE="TEXT" NAME="user_name" maxlength="40" size="30" /></TD>
+					<TD>Username</TD><TD>: <INPUT TYPE="TEXT" NAME="user_name" ID="username" ""maxlength="40" size="30" /></TD>
 				</TR>
 				<TR>
-					<TD>Password</TD><TD>: <INPUT TYPE="PASSWORD" NAME="pass" maxlength="40" size="30" /></TD>
+					<TD>Password</TD><TD>: <INPUT TYPE="PASSWORD" NAME="password" ID="password" maxlength="15" size="30" /></TD>
 				</TR>
 				<TR>
 					<TD colspan="2">
 					<INPUT class="btn" TYPE="reset" NAME="Reset" VALUE="Reset" />
 					<INPUT class="btn" TYPE="submit" NAME="Login" VALUE="Login" />
+					</TD>
+				</TR>
+				<TR>
+					<TD COLSPAN="2">
+					<?php
+					
+					if(isset($_POST['Login']))
+					{
+						echo '<SPAN ID="err_msg">Invalid Username/Password</SPAN>';
+					}	
+					?>
 					</TD>
 				</TR>
 				</TABLE>
