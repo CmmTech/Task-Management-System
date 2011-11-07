51e6c1f5390a1870984f592ac2a934a158dc4a47
shine Chandran <shine@cmmtechnologies.in>
2011-11-07 10:49:44 +0530

    modified login.css,login.js,index.php

--- index.php
+++ index.php
@@ -8,12 +8,10 @@ if(isset($_POST['Login']))
 	if(checkLogin($username,$password,$connection))
 	{
 		//header(""); 
-		echo  "yooooooooooooo";
+		echo  "yooooooooooooo! Login sucessfull!";
 		die();
 	}
 	
-
-
 }
 
 ?>
@@ -23,7 +21,7 @@ if(isset($_POST['Login']))
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <HTML>
 	<HEAD>
-		<TITLE>LOGIN PAGE</TITLE>
+		<TITLE>Task Management System - Log In</TITLE>
 		<link rel="stylesheet" href="login.css">	
 		<SCRIPT TYPE="TEXT/JAVASCRIPT" SRC="jquery-1.6.2.min.js"></SCRIPT>
 	    <SCRIPT TYPE="TEXT/JAVASCRIPT" SRC="login.js"></SCRIPT>
@@ -35,30 +33,40 @@ if(isset($_POST['Login']))
 				<IMG SRC="images/login_lock.png" width="60px" height="60px" />
 				<H2>Login</H2>
 			</DIV>
-			<FORM NAME="log" ACTION="" METHOD="POST">
+			<FORM NAME="log" ID="log" ACTION="" METHOD="POST">
 				
 				<TABLE cellpadding="4">
 				<TR>
-					<TD>Username</TD><TD>: <INPUT TYPE="TEXT" NAME="user_name" ID="username" ""maxlength="40" size="30" /></TD>
+					<TD>
+						Username
+					</TD>
+					<TD>
+						: <INPUT TYPE="TEXT" NAME="user_name" ID="username" maxlength="40" size="30" />
+					</TD>
 				</TR>
 				<TR>
 					<TD>Password</TD><TD>: <INPUT TYPE="PASSWORD" NAME="password" ID="password" maxlength="15" size="30" /></TD>
 				</TR>
 				<TR>
 					<TD colspan="2">
-					<INPUT class="btn" TYPE="reset" NAME="Reset" VALUE="Reset" />
-					<INPUT class="btn" TYPE="submit" NAME="Login" VALUE="Login" />
+					<INPUT class="btn" TYPE="submit" ID="login" NAME="Login" VALUE="Login" />
+					<INPUT class="btn" TYPE="reset" ID="reset" NAME="Reset" VALUE="Reset" />					
 					</TD>
 				</TR>
 				<TR>
 					<TD COLSPAN="2">
-					<?php
-					
+					<?php					 					
 					if(isset($_POST['Login']))
 					{
-						echo '<SPAN ID="err_msg">Invalid Username/Password</SPAN>';
-					}	
+						echo "<SPAN ID='err_span' class='err_span err_border'>Invalid Username/Password</SPAN>";
+					}
+					else
+					{
+						echo "<SPAN ID='err_span'class='err_span'></span>";
+					}
+
 					?>
+					
 					</TD>
 				</TR>
 				</TABLE>
--- login.js
+++ login.js
@@ -1,3 +1,42 @@
 $(document).ready(function(){
 	$("#username").focus();
+	
+	flag = true;
+	$("#log").submit(function(){
+		//alert(1111);
+
+		if($("#username").val()=="" && $("#password").val()=="")
+		{
+			$("#err_span").addClass('err_border').html("Enter a valid username and password");
+			flag = false;
+		}
+		else if($("#username").val()=="" || $("#password").val()=="")
+		{
+			$("#err_span").addClass('err_border').html("Either username/password is incorrect");
+			flag = false;
+		}
+		else if($("#username").val()!="" && $("#password").val()!="")
+		{
+			
+			flag=true;
+		}
+		
+		if(flag)
+		{
+			return true;
+		}
+		else
+		{
+			return false;
+		}
+
+			
+	});
+
+
+	$("#reset").click(function(){
+		$("#err_span").html("");
+		$("#err_span").removeClass("err_border");
+	});
+	
 });
